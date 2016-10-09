913a24dc57d5aefd3f4802abc2a923ae0213a709
Robert Marin <triplehalo@gmail.com>
2016-10-09 13:04:12 -0700

    Update todaysDate.php

--- lib/todaysDate.php
+++ lib/todaysDate.php
@@ -2,7 +2,7 @@
 // This function just creates today's date in a naturally SQL friendly format.
 // But it will also take a string parameter to format date and time if you want to do that.
 // This tends to save me some time though.
-function todays_date($s = "Y-m-d") [
+function todays_date($s = "Y-m-d") {
 	return date($s);
 }
 ?>
