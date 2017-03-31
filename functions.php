497b97ea1857caa1f1f1384e23ef75c7f72dad1b
Robert Marin <triplehalo@gmail.com>
2017-03-30 21:07:02 -0700

    A app to

--- functions.php
+++ functions.php
@@ -20,4 +20,42 @@ throw new Exception(curl_error($ch), curl_errno($ch));
 curl_close($ch); //Close handle
 return $output; //Return contents
 }
+
+function merge_array($r) {
+    $merge = array();
+    foreach( $r as $word ) {
+        //echo $word . "<br/>";
+        $merge[$word]++;
+    }
+    return $merge;
+}
+
+function print_array($r, $conn) {
+    arsort($r);
+    echo "<table><tr></tr><th>Word</th><th>Count</th><th>Type</th></tr>";
+    foreach ($r as $word => $count) {
+        $results = $conn->query("SELECT word, wordtype FROM entries WHERE word = '" . $word . "' AND wordtype != ''");
+        echo $conn->error; // Echo error if necessary.
+
+        $row = mysqli_fetch_object($results);
+        $type = $row->wordtype;
+        echo "<td><a href='dictionary.php?word=" . $word . "'>" . $word . "</a></td><td> " . $count . "</td><td>" . $type . "</td><tr/>";
+    }
+    echo "</table>";
+}
+
+function show_dom_node(DOMNode $domNode) {
+    if(!isset($tag_array)) { $tag_array = array(); }
+    foreach ($domNode->childNodes as $node)
+    {
+        //echo "<div>" . htmlspecialchars($node->nodeName.':'.$node->nodeValue) . "</div>";
+        $tag_array[$node->nodeName]++;
+        if($node->hasChildNodes()) {
+            show_dom_node($node);
+        }
+    }
+
+    return "hello";
+}
+
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <style>
+    body {
+        font-family: verdana;
+        font-size: 10px;
+    }
     td {
         border: solid 1px black;
     }
@@ -7,31 +11,18 @@
 <?php
 require_once("env.php");
 require_once("functions.php");
+require_once("classes.php");
 $conn = new mysqli($dbr[0], $dbr[1], $dbr[2], $dbr[3]); // New DB Connection
 echo $conn->error; // Echo error if necessary.
 
 $url = "http://www.investors.com/category/market-trend/stock-market-today/";
-$test = url_get_contents($url);
+$html = url_get_contents($url);
 //var_dump($test);
 
-$s = "Today’s 20-somethings and even those in their early 30s came of age in what may have been the worst national real estate market on record since their grandparents were born. Now a major real estate ad company wants to persuade them that it’s high time to take a risk; that it’s time to walk away from paying high rents in some costly markets and pay high prices to buy their own homes.";
-$word_array = explode(" ", $s);
-$sorted_array = array();
-foreach( $word_array as $word ) {
-    //echo $word . "<br/>";
-    $sorted_array[$word]++;
-}
+$dom = new DOMDocument;
+$dom->loadHTML($html);
 
-arsort($sorted_array);
-echo "<table><tr></tr><th>Word</th><th>Count</th><th>Type</th></tr>";
-foreach($sorted_array as $word => $count) {
-    $results = $conn->query( "SELECT word, wordtype FROM entries WHERE word = '" . $word . "' AND wordtype != ''");
-    echo $conn->error; // Echo error if necessary.
-
-    $row = mysqli_fetch_object($results);
-    $type = $row->wordtype;
-    echo "<td><a href='dictionary.php?word=" . $word . "'>" . $word . "</a></td><td> " . $count . "</td><td>" . $type . "</td><tr/>";
-}
-echo "</table>";
+$tag_array = array();
+$test = show_dom_node($dom,$tag_array);
 
 ?>
