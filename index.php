97d29874d29176f74141e35040d3193e0a7e87e1
Robert Marin <triplehalo@gmail.com>
2017-04-02 10:47:06 -0700

    Quick link search engine created

--- index.php
+++ index.php
@@ -9,6 +9,16 @@
     .link_name {
         width: 15%;
     }
+    #search{
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 20px;
+        position: static;
+        width: 400px;
+    }
+    #search_form {
+        width: 100%;
+    }
 </style>
 
 <?php
@@ -22,33 +32,44 @@ require_once("library/Zend/Dom/Document/Query.php");
 //echo $conn->error; // Echo error if necessary.
 use Zend\Dom\Query;
 
-$url = [ 'g' => 'http://www.google.com',
-            't' => 'http://www.investors.com/category/market-trend/stock-market-today/'
-        ];
-$html = url_get_contents ( $url['t'] );
 
-$dom = new Query($html);
-$results = $dom->execute('a');
+?>
 
-$cols = ["Link Text","Address","Count"];
+<div id="search">
+    <form method="get">
+        <input id="search_form" type="search" name="url" placeholder=" <?= ( isset( $_GET['url'] ) ? "Address: " .  $_GET['url'] : 'Enter A URL' ) ?> "/>
+    </form>
+</div>
 
-$r= Array();
-foreach($results as $result) {;
-    if(!isset($r[$result->getAttribute('href')]['count'])) { $r[$result->getAttribute('href')]['count'] = 0; }
-    $r[$result->getAttribute('href')]['count']++;
-    if( in_array( $result->textContent, $r[$result->getAttribute('href')]['text'] ) ) { continue; }
-    $r[$result->getAttribute('href')]['text'][] = $result->textContent;
-    //echo print_table_rows( [ $result->getAttribute('href'), $result->textContent ] );
-}
+<?php
+if(isset($_GET['url'])) {
+    //$url = ['g' => 'http://www.google.com', 't' => 'http://www.investors.com/category/market-trend/stock-market-today/'];
+    $html = url_get_contents($_GET['url']);
+    $dom = new Query($html);
+    $results = $dom->execute('a');
+
+    $cols = ["Link Text", "Address", "Count"];
+
+    $r = Array();
+    foreach ($results as $result) {
+        ;
+        if (!isset($r[$result->getAttribute('href')]['count'])) {
+            $r[$result->getAttribute('href')]['count'] = 0;
+        }
+        $r[$result->getAttribute('href')]['count']++;
+        if (in_array($result->textContent, $r[$result->getAttribute('href')]['text'])) {
+            continue;
+        }
+        $r[$result->getAttribute('href')]['text'][] = $result->textContent;
+        //echo print_table_rows( [ $result->getAttribute('href'), $result->textContent ] );
+    }
 //var_dump($r);
 
-array_sort_by_column($r, 'count');
-
-echo open_table( $cols );
-foreach($r as $k => $v) {
-    echo print_table_rows( [ $k, $v['text'], $v['count'] ] );
+    array_sort_by_column($r, 'count', SORT_DESC);
+    echo open_table($cols);
+    foreach ($r as $k => $v) {
+        echo print_table_rows([$k, $v['text'], $v['count']]);
+    }
+    echo close_table();
 }
-
-echo close_table();
-
 ?>
